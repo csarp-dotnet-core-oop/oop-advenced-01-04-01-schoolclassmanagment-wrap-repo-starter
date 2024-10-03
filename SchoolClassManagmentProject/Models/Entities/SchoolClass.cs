1a73c228e277849371578acccde22b9773f4bf96
gycsaba <gycsaba@SZT9-24>
2024-10-03 14:53:40 +0200

    A kész projekt

---
+++ SchoolClassManagmentProject/Models/Entities/SchoolClass.cs
@@ -0,0 +1,63 @@
+﻿using SchoolClassManagmentProject.Models.AppExceptions;
+
+namespace SchoolClassManagmentProject.Models.Entities
+{
+    public class SchoolClass
+    {
+        private byte _grade;
+        private char _gradeLetter;
+        private byte _lastGrade;
+        private int _classMoney;
+        public SchoolClass()
+        {
+            _grade = byte.MinValue;
+            _gradeLetter = char.MinValue;
+            _classMoney = int.MinValue;
+        }
+
+        public SchoolClass(byte grade, char gradeLetter, byte lastGrade)
+        {
+            _grade = grade;
+            _gradeLetter = gradeLetter;
+            _lastGrade = lastGrade;
+            _classMoney = int.MinValue;
+        }
+
+        public byte Grade { get => _grade; set => _grade = value; }
+        public char GradeLetter { get => _gradeLetter; set => _gradeLetter = value; }
+        public byte LastGrade { get => _lastGrade; private set => _lastGrade = value; }
+        public int ClassMoney { get => _classMoney; private set => _classMoney = value; }
+        public string Name => $"{_grade}. {_gradeLetter}";
+        public bool HasGraduated => _grade > _lastGrade;
+        public bool IsGraduate => _grade == _lastGrade;
+        public bool NotGraduate => !HasGraduated;
+
+        public void SetClassMoney(int classMoney)
+        {
+            _classMoney = classMoney; 
+        }
+        public void SetLastGrade(byte newLastGrade)
+        {
+            if (newLastGrade > _grade)
+                LastGrade = newLastGrade;
+            else
+                throw new LastGradeModificationErrorException($"{nameof(SchoolClass)} osztályba, {nameof(SetLastGrade)} metódusban paraméter hiba történt.", nameof(newLastGrade),null);
+        }
+
+        public void AdvanceGrade()
+        {
+            if (NotGraduate)
+                Grade = (byte) (Grade + 1);
+        }
+
+        ~SchoolClass()
+        {
+            Console.WriteLine($"{_grade}. {_gradeLetter} osztály megszünt.");
+        }
+
+        public override string ToString()
+        {
+            return $"{Grade}.{GradeLetter}";
+        }
+    }
+}
---
+++ SchoolClassManagmentProject/Program.cs
@@ -0,0 +1,70 @@
+﻿using SchoolClassManagmentProject.Models.AppExceptions;
+using SchoolClassManagmentProject.Models.Entities;
+using SchoolClassManagmentProject.Repos;
+
+SchoolClass schoolClass9a = new SchoolClass(9, 'a',12);
+SchoolClass schoolClass9b = new SchoolClass(9, 'b',13);
+SchoolClass schoolClass10a = new SchoolClass(10, 'a', 12);
+SchoolClass schoolClass10b = new SchoolClass(10, 'b', 12);
+
+SchoolClass schoolClass12a = new SchoolClass(12, 'a', 12);
+SchoolClass schoolClass12b = new SchoolClass(12, 'b', 12);
+
+SchoolClass schoolClass13a = new SchoolClass(13, 'a',13);
+SchoolClass schoolClass13b = new SchoolClass(13, 'b',13);
+
+try
+{
+    schoolClass9a.SetLastGrade(1);
+}
+catch (LastGradeModificationErrorException lastGradeModificationError)
+{
+    Console.WriteLine(lastGradeModificationError.ParamName);
+    Console.WriteLine(lastGradeModificationError.Message);
+}
+
+schoolClass9a.AdvanceGrade();
+Console.WriteLine(schoolClass9a.Name);
+
+SchoolClassRepo schoolClassRepo = new SchoolClassRepo();
+schoolClass10b.SetClassMoney(1500);
+schoolClassRepo.Add(schoolClass9a);
+schoolClassRepo.Add(schoolClass9b);
+schoolClassRepo.Add(schoolClass10a);
+schoolClassRepo.Add(schoolClass10b);
+schoolClassRepo.Add(schoolClass12a);
+schoolClassRepo.Add(schoolClass12b);
+schoolClassRepo.Add(schoolClass13a);
+schoolClassRepo.Add(schoolClass13b);
+
+Console.WriteLine($"Iskolában az osztályok száma: {schoolClassRepo.NumberOfSchoolClasses}");
+Console.WriteLine($"Iskolában a végzős osztályok száma: {schoolClassRepo.NumberOfGraduateClasses}");
+
+Console.WriteLine("Végzős osztályok:");
+List<SchoolClass> gradeteClasses=schoolClassRepo.GetGraduateClasses();
+foreach(SchoolClass graduateClass in gradeteClasses)
+    Console.WriteLine(graduateClass);
+
+
+
+int exsistingClassClassMoneyPerStudentInOneYear = schoolClassRepo.GetClassMoneyPerStudentInOneYear(10, 'b');
+Console.WriteLine($"10.a osztáybevétele egy évben egy diák által:{exsistingClassClassMoneyPerStudentInOneYear}");
+
+int nonExsistingClassClassMoneyPerStudentInOneYear = schoolClassRepo.GetClassMoneyPerStudentInOneYear(-1, 'z');
+Console.WriteLine($"10.a osztáybevétele egy évben egy diák által:{nonExsistingClassClassMoneyPerStudentInOneYear}");
+
+Console.WriteLine("Diákok osztályonként");
+Dictionary<byte, int> gradeCount = schoolClassRepo.GetSchoolClassCountByGrade();
+
+// kulcsokon végigmenni
+// foreach (byte grade in gradeCount.Keys) ;
+
+// értékeken végigmennei
+// foreach(int count in gradeCount.Values)
+
+/*
+foreach(KeyValuePair<byte,int> grade in gradeCount)
+    Console.WriteLine($"{grade.Key} évfolyamon {grade.Value} osztály van.");
+*/
+foreach (var grade in gradeCount)
+    Console.WriteLine($"{grade.Key} évfolyamon {grade.Value} osztály van.");
