2a3e71b3211e6f1c445c8883345257f303b4b32c
gycsaba <gycsaba@SZT9-24>
2024-10-07 17:37:28 +0200

    Kész a feladat

--- SchoolClassManagmentProject/Program.cs
+++ SchoolClassManagmentProject/Program.cs
@@ -68,3 +68,43 @@ foreach(KeyValuePair<byte,int> grade in gradeCount)
 */
 foreach (var grade in gradeCount)
     Console.WriteLine($"{grade.Key} évfolyamon {grade.Value} osztály van.");
+
+// (StudentRepo)
+StudentRepo studentRepo = new();
+studentRepo.Add(new("Alma", "Piros", "1@mail.org", "123", DateTime.Now.AddYears(-21).AddMonths(11), true));
+studentRepo.Add(new("Körte", "Sárga", "2@mail.org", "456", DateTime.Now.AddYears(-14).AddMonths(9), false));
+studentRepo.Add(new("Szilva", "Lila", "3@mail.org", "789", DateTime.Now.AddYears(-8).AddMonths(8), false));
+studentRepo.Add(new("Lajos", "Mari", null, "101", DateTime.Now.AddYears(-12).AddMonths(7), true));
+studentRepo.Add(new("Nagy", "Melinda", "5@mail.org", "121", DateTime.Now.AddYears(-10).AddMonths(1), true));
+studentRepo.Add(new("Dél", "Panni", "6@mail.org", "131", DateTime.Now.AddYears(-13).AddMonths(2), false));
+studentRepo.Add(new("Magyar", "Péter", "7@mail.org", "141", DateTime.Now.AddYears(-14).AddMonths(3), true));
+studentRepo.Add(new("Németh", "Márton", null, "151", DateTime.Now.AddYears(-17).AddMonths(4), true));
+studentRepo.Add(new("Lila", "Virág", "9@mail.org", "161", DateTime.Now.AddYears(-14).AddMonths(10), false));
+studentRepo.Add(new("Szép", "Dália", "10@mail.org", "171", DateTime.Now.AddYears(-18).AddMonths(11), false));
+
+Console.WriteLine("E-mail címmel nem rendelkező diákok: ");
+foreach (var s in studentRepo.StudentsWithoutEmail)
+    Console.WriteLine(" - " + s.HungarianFullName);
+Console.WriteLine("E-mail címmel nem rendelkező diákok száma: " + studentRepo.NumberOfStudentsWithoutEmail);
+Console.WriteLine("Fiúk: " + studentRepo.NumberOfMalesAndFemales()["males"]);
+Console.WriteLine("Lányok: " + studentRepo.NumberOfMalesAndFemales()["females"]);
+Console.WriteLine("Felnőtt diákok:");
+foreach (var s in studentRepo.AdultStudents)
+    Console.WriteLine(" - " + s.HungarianFullName);
+Console.WriteLine("Felnőtt diákok száma: " + studentRepo.NumberOfAdultStudents);
+Console.WriteLine("2024-ben született diákok:");
+foreach (var s in studentRepo.StudentsBornInYear(2024))
+    Console.WriteLine(" - " + s.HungarianFullName);
+Console.WriteLine("Ebben a hónapban született diákok:");
+foreach (var s in studentRepo.StudentsBornInMonth(DateTime.Now.Month))
+    Console.WriteLine(" - " + s.HungarianFullName);
+
+Student newStudent = new("Negye", "Dóra", "dorika@mail.org", "987", new DateTime(2001, 5, 2), false);
+studentRepo.Add(newStudent);
+Console.WriteLine(newStudent.HungarianFullName);
+studentRepo.SetFirstName("Dóra", "Negye", "Reide");
+Console.WriteLine(newStudent.HungarianFullName);
+
+Console.WriteLine(studentRepo.StudentsBornInMonth(5).Count);
+studentRepo.RemoveStudentWithMatchingName("Reide", "Negye"); // Törlés
+Console.WriteLine(studentRepo.StudentsBornInMonth(5).Count); 
---
+++ SchoolClassManagmentProject/Repos/StudentRepoStatistics.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolClassManagmentProject.Repos
+{
+    public partial class StudentRepo
+    {
+        public int NumberOfStudentsWithoutEmail => StudentsWithoutEmail.Count;
+        public int NumberOfAdultStudents => AdultStudents.Count;
+    }
+}
